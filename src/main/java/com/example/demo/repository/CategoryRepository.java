7fb58282fb17b3c9bb8775bb6fac077dc46f2d29
ducanh1997 <ducanh8098@gmail.com>
2023-04-19 08:28:14 +0700

    fix category repository

--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -3,13 +3,13 @@ import com.example.demo.Model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 @Repository
 @Transactional
 public interface CategoryRepository extends JpaRepository<Category,Long> {
-
     @Query(value = "select c from Category c where c.typeCategory = 'expense'")
     List<Category> findAllByExpense();
-
     @Query(value = "select c from Category c where c.typeCategory = 'income'")
     List<Category> findAllByIncome();
 }
