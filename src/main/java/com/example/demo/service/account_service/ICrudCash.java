c1e1023eb424ede25840af8e42a38f272163afe3
ducanh1997 <ducanh8098@gmail.com>
2023-04-19 09:06:36 +0700

    update all

--- src/main/java/com/example/demo/service/account_service/ICrudCash.java
+++ src/main/java/com/example/demo/service/account_service/ICrudCash.java
@@ -3,6 +3,7 @@ import com.example.demo.Model.Cash;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,6 @@ public interface ICrudCash {
     void save(Cash cash);
     void delete(Long id);
     Page<Cash> findCashByIdUser(Pageable pageable, Optional<Long> userId);
-    List<Cash> findCashByDate(Long userId, LocalDateTime startDate,LocalDateTime endDate);
+    List<Cash> findCashByDate(Long userId, LocalDate startDate, LocalDate endDate);
 
 }
